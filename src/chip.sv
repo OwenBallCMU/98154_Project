a278829a6d09674c2c1e0601470e4d55b987297a
OwenBallCMU <oball@andrew.cmu.edu>
2023-04-11 01:22:16 -0400

    updating chip interface

--- src/chip.sv
+++ src/chip.sv
@@ -7,41 +7,24 @@ module my_chip (
     input logic reset // Important: Reset is ACTIVE-HIGH
 );
     
-    // Basic counter design as an example
+    logic SCL_in, SDA_in, SDA_out;
+    logic [7:0] reg_out;
+    logic [255:0] registers_packed;
+    logic clock, SDA_out_temp, in_wait;
 
+    logic [10:0] data_out;
+    logic [9:0] data_in;
 
-    wire [6:0] led_out;
-    assign io_out[6:0] = led_out;
+    assign SCL_in = io_in[0];
+    assign SDA_in = io_in[1];
+    I2C M1 (.SCL_in, .SDA_in, .clock, .reset, .SDA_out(SDA_out_temp), .reg_out, .in_wait, .registers_packed);
 
-    // external clock is 1000Hz, so need 10 bit counter
-    reg [9:0] second_counter;
-    reg [3:0] digit;
+    IO  M2 (.registers_packed, .data_in, .data_out, .clock, .reset);
 
-    always @(posedge clock) begin
-        // if reset, set counter to 0
-        if (reset) begin
-            second_counter <= 0;
-            digit <= 0;
-        end else begin
-            // if up to 16e6
-            if (second_counter == 1000) begin
-                // reset
-                second_counter <= 0;
+    assign SDA_out = ~SDA_out_temp;
 
-                // increment digit
-                digit <= digit + 1'b1;
+    assign io_out[11:1] = data_out;
+    assign io_out[0] = SDA_out; 
 
-                // only count from 0 to 9
-                if (digit == 9)
-                    digit <= 0;
-
-            end else
-                // increment counter
-                second_counter <= second_counter + 1'b1;
-        end
-    end
-
-    // instantiate segment display
-    seg7 seg7(.counter(digit), .segments(led_out));
 
 endmodule
--- src/toolchain.sh
+++ src/toolchain.sh
@@ -4,4 +4,4 @@ set -e
 yosys -p 'read_verilog -sv *.sv; synth_ice40 -json build/synthesis.json -top chipInterface; stat'
 nextpnr-ice40 --hx8k --json build/synthesis.json --asc build/pnr.asc --package cb132 --pcf constraints.pcf --freq 100 --pre-place constrain.py
 icepack build/pnr.asc build/bitstream.bit
-yosys -p 'read_verilog -sv *.sv; synth -top chipInterface; stat'
+yosys -p 'read_verilog -sv *.sv; synth -top my_chip; stat'
