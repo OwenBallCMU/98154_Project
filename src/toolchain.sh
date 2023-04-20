7cad9c26ce454ccd987134cb1e7b985ac81912d3
OwenBallCMU <oball@andrew.cmu.edu>
2023-04-20 15:27:48 -0400

    merged

--- src/toolchain.sh
+++ src/toolchain.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #./toolchain 
-files='top.sv datapath.sv IO.sv FSM.sv library.sv'
+files='chip.sv top.sv datapath.sv IO.sv FSM.sv library.sv'
 set -e
 yosys -p "read_verilog -sv $files; synth_ice40 -json build/synthesis.json -top chipInterface; stat"
 nextpnr-ice40 --hx8k --json build/synthesis.json --asc build/pnr.asc --package cb132 --pcf constraints.pcf --freq 100 --pre-place constrain.py
 icepack build/pnr.asc build/bitstream.bit
-yosys -p "read_verilog -sv $files; synth -top chipInterface; stat"
+yosys -p "read_verilog -sv $files; synth -top my_chip; stat"
