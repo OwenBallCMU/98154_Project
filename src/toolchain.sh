8359dec93e8f47da2c0ffe04e40f474f39566869
OwenBallCMU <oball@andrew.cmu.edu>
2023-04-20 18:02:55 -0400

    Tested input reg and running my_chip from top

--- src/toolchain.sh
+++ src/toolchain.sh
@@ -5,4 +5,4 @@ set -e
 yosys -p "read_verilog -sv $files; synth_ice40 -json build/synthesis.json -top chipInterface; stat"
 nextpnr-ice40 --hx8k --json build/synthesis.json --asc build/pnr.asc --package cb132 --pcf constraints.pcf --freq 90 --pre-place constrain.py
 icepack build/pnr.asc build/bitstream.bit
-yosys -p "read_verilog -sv $files; synth -top chipInterface; stat"
+yosys -p "read_verilog -sv $files; synth -top my_chip; stat"
